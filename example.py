ad33b2f73e9fb537abab895b8dd78f5d4433f35d
DenisTimer <denis1timerkaev@gmail.com>
2026-07-05 10:49:15 +0300

    add linter ruff

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from more_itertools import flatten, substrings
 
 coll = [(0, 1), (2, 3)]
 # делаем коллекцию плоской
-print(list(flatten(coll))) # => [0, 1, 2, 3]
+print(list(flatten(coll)))  # => [0, 1, 2, 3]
 
 # получаем все подстроки строки
 print([''.join(s) for s in substrings('more')])
